a3204f4a69d5fec3804967844e5397e9f0f80bfc
Manda Jensen <phadkem@oregonstate.edu>
2020-11-05 16:19:23 -0600

    Add Currency table to db and test db is working

---
+++ src/main/java/net/devdiaries/wallet/domain/CurrencyRepository.java
@@ -0,0 +1,6 @@
+package net.devdiaries.wallet.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CurrencyRepository extends CrudRepository<Currency, Long> {
+}
